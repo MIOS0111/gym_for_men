e8749edf2eae8cbec30cad2e7d9839aae1a376da
MIOS0111 <mihailsemakin.2004@gmail.com>
2024-12-27 16:47:47 +0500

    111

--- src/pages/Scripts.module.js
+++
@@ -1,8 +0,0 @@
-export default function alert_html_val(){
-    const name = document.getElementById('name').value;
-    const tel = document.getElementById('tel').value;
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-    const mail = document.getElementById('mail').value;
-    alert(`Вы записались на занятие\nИмя: ${name}\nТелефон: ${tel}\nДата: ${date} \nВремя: ${time}\nEmail: ${mail}`);
-}
